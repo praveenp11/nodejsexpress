bfc0e1a94dc588c08f5155076f0f0c6753caa0b2
Sivagopal <siva@votigo.com>
2018-03-29 17:29:10 +0530

    adding extra headers

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE,HEAD');
   
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
