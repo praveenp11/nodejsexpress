2bc77eca0322e94e0b0b9ad5e563e40ca197e798
Sivagopal <siva@votigo.com>
2018-04-04 16:31:06 +0530

    socket changes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-console.log(process.env);
+// console.log(process.env);
 var db = mongoose.connect(process.env.MONGO_DB_HOST || 'mongodb://localhost:27017/test');
 module.exports = db;
